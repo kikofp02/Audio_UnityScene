4ec5142cfcf0a14f10ee2a427e125a00cc470ed1
Francisco Ferrerós Puigmarti <99959433+kikofp02@users.noreply.github.com>
2024-05-02 19:04:17 +0200

    Door Script

---
+++ Audio_UnityScene/Assets/Audio_Project/Scripts/DoorCycleScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCycleScript : MonoBehaviour
+{
+    Animator animator;
+    GameObject[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = 0;
+        bool door_state = animator.GetBool("character_nearby");
+
+        foreach (GameObject player in players)
+        {
+            distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+            if (distance < 3.0)
+            {
+                animator.SetBool("character_nearby", true);
+                break;
+            }
+            else if (animator.GetBool("character_nearby") == true)
+            {
+                animator.SetBool("character_nearby", false);
+            }
+        }
+
+        if(animator.GetBool("character_nearby") != door_state)
+        {
+            if (animator.GetBool("character_nearby") == true)
+            {
+                //Sound fx to open
+            }
+            else
+            {
+                //Sound fx to close
+            }
+        }
+    }
+}
